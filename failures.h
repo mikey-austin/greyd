96278555d5659a602e2494773cbc6a076b038330
Mikey Austin <mikey@mikeyaustin.com>
2014-06-29 14:07:49 +1000

    added some documentation to failures.h

--- failures.h
+++ failures.h
@@ -3,6 +3,9 @@
  * @brief  Defines program-wide functions to handle failures.
  * @author Mikey Austin
  * @date   2014
+ *
+ * All of the error handling functions below log/print their messages, and
+ * may exit depending on their severity.
  */
 
 #ifndef FAILURES_DEFINED
@@ -22,8 +25,24 @@
 #define I_INFO(msg, ...)\
     i_info(__FILE__, __LINE__, msg, __VA_ARGS__)
 
+/**
+ * The critical info function will log it's message and exit.
+ */
 extern void i_critical(const char *file, const int line, const char *msg, ...);
+
+/**
+ * The error info function will log it's message and exit.
+ */
 extern void i_error(const char *file, const int line, const char *msg, ...);
+
+/**
+ * The warning info function will just log it's message.
+ */
 extern void i_warning(const char *file, const int line, const char *msg, ...);
+
+/**
+ * The info function will just log it's message.
+ */
 extern void i_info(const char *file, const int line, const char *msg, ...);
+
 #endif
