38225ed045cfa1c09b6c3f93e32bf0986d21dd02
Mikey Austin <mikey@ayuda.com.au>
2014-12-03 09:13:54 +1100

    added new versions of sstrncpy from the new OpenBSD strlcpy, same for sstrncat

--- utils.c
+++ utils.c
@@ -3,58 +3,53 @@
  * @brief  Support utilities for use throughout the system.
  */
 
-#include <errno.h>
+/*
+ * Copyright (c) 1998, 2014 Todd C. Miller <Todd.Miller@courtesan.com>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 #include <stdlib.h>
-#include <stdio.h>
-#include <stdarg.h>
 #include <string.h>
 
-/**
- * A "safe" strncpy, saves room for \0 at end of dest, and refuses to copy
- * more than "n" bytes.
- *
- * Taken from proftpd source.
- */
-extern int
-sstrncpy(char *dst, const char *src, size_t n)
+extern size_t
+sstrncat(char *dst, const char *src, size_t dsize)
 {
-#ifndef HAVE_STRLCPY
-    register char *d = dst;
-#endif /* HAVE_STRLCPY */
-    int res = 0;
+    const size_t slen = strlen(src);
+    const size_t dlen = strnlen(dst, dsize);
 
-    if(dst == NULL) {
-        errno = EINVAL;
-        return -1;
+    if(dlen != dsize) {
+        size_t count = slen;
+        if(count > dsize - dlen - 1)
+            count = dsize - dlen - 1;
+        dst += dlen;
+        memcpy(dst, src, count);
+        dst[count] = '\0';
     }
 
-    if(src == NULL) {
-        errno = EINVAL;
-        return -1;
+    return slen + dlen;
+}
+
+size_t
+sstrncpy(char *dst, const char *src, size_t dsize)
+{
+    const size_t slen = strlen(src);
+
+    if(dsize != 0) {
+        const size_t dlen = dsize > slen ? slen : dsize - 1;
+        memcpy(dst, src, dlen);
+        dst[dlen] = '\0';
     }
 
-    if(n == 0) {
-        return 0;
-    }
-
-#ifdef HAVE_STRLCPY
-    strlcpy(dst, src, n);
-
-    /* We want the returned length to be the number of bytes copied as
-     * requested by the caller, not the total length of the src string.
-     */
-    res = n;
-
-#else
-    if(src && *src) {
-        for(; *src && n > 1; n--) {
-            *d++ = *src++;
-            res++;
-        }
-    }
-
-    *d = '\0';
-#endif /* HAVE_STRLCPY */
-
-    return res;
+    return slen;
 }
--- utils.h
+++ utils.h
@@ -10,6 +10,20 @@
 
 #include <stdlib.h>
 
-extern int sstrncpy(char *dst, const char *src, size_t n);
+/**
+ * Appends src to string dst of size dsize (unlike strncat, dsize is the
+ * full size of dst, not space left).  At most dsize-1 characters
+ * will be copied.  Always NUL terminates (unless dsize <= strlen(dst)).
+ * Returns strlen(src) + MIN(dsize, strlen(initial dst)).
+ * If retval >= dsize, truncation occurred.
+ */
+extern size_t sstrncat(char *dst, const char *src, size_t dsize);
+
+/**
+ * Copy src to string dst of size dsize.  At most dsize-1 characters
+ * will be copied.  Always NUL terminates (unless dsize == 0).
+ * Returns strlen(src); if retval >= dsize, truncation occurred.
+ */
+extern size_t sstrncpy(char *dst, const char *src, size_t dsize);
 
 #endif
