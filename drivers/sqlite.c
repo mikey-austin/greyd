fc9ca26d0ba53d5eb4d5e4d94b043ab98fda5bbd
Mikey Austin <mikey@jackiemclean.net>
2015-03-21 20:09:58 +0900

    completed (untested) sqlite driver implementation

--- drivers/sqlite.c
+++ drivers/sqlite.c
@@ -51,7 +51,10 @@ struct s3_handle {
 struct s3_itr {
     sqlite3_stmt *stmt;
     struct DB_key *curr;
-}
+};
+
+static void populate_key(sqlite3_stmt *, struct DB_key *, int);
+static void populate_val(sqlite3_stmt *, struct DB_val *, int);
 
 extern void
 Mod_db_init(DB_handle_T handle)
@@ -314,12 +317,179 @@ err:
 extern int
 Mod_db_get(DB_handle_T handle, struct DB_key *key, struct DB_val *val)
 {
-    return GREYDB_ERR;
+    struct s3_handle *dbh = handle->dbh;
+    sqlite3_stmt *stmt;
+    char *sql;
+    struct Grey_tuple *gt;
+    int ret, res;
+
+    switch(key->type) {
+    case DB_KEY_MAIL:
+        sql = "SELECT 0, 0, 0, 0, -2 "
+            "FROM spamtraps WHERE `address`='?' "
+            "LIMIT 1";
+        ret = sqlite3_prepare(dbh->db, sql, sizeof(sql), &stmt, NULL);
+        if(ret != SQLITE_OK) {
+            i_warn("sqlite3_prepare: %s", sqlite3_errstr(ret));
+            goto err;
+        }
+
+        ret = sqlite3_bind_text(stmt, 1, key->data.s, -1, SQLITE_STATIC);
+        if(ret != SQLITE_OK) {
+            i_warn("sqlite3_bind_text: %s", sqlite3_errstr(ret));
+            goto err;
+        }
+        break;
+
+    case DB_KEY_IP:
+        sql = "SELECT `first`, `pass`, `expire`, `bcount`, `pcount`"
+            "FROM entries "
+            "WHERE `ip`='?' AND `helo` IS NULL AND `from` IS NULL "
+            "AND `to` IS NULL "
+            "LIMIT 1";
+        ret = sqlite3_prepare(dbh->db, sql, sizeof(sql), &stmt, NULL);
+        if(ret != SQLITE_OK) {
+            i_warn("sqlite3_prepare: %s", sqlite3_errstr(ret));
+            goto err;
+        }
+
+        if(sqlite3_bind_text(stmt, 1, key->data.s, -1, SQLITE_STATIC)
+            != SQLITE_OK)
+        {
+            i_warn("sqlite3_bind_*: %s", sqlite3_errmsg(dbh->db));
+            goto err;
+        }
+        break;
+
+    case DB_KEY_TUPLE:
+        sql = "SELECT `first`, `pass`, `expire`, `bcount`, `pcount`"
+            "FROM entries "
+            "WHERE `ip`='?' AND `helo`='?' AND `from`='?' AND `to`='?' "
+            "LIMIT 1";
+        ret = sqlite3_prepare(dbh->db, sql, sizeof(sql), &stmt, NULL);
+        if(ret != SQLITE_OK) {
+            i_warn("sqlite3_prepare: %s", sqlite3_errstr(ret));
+            goto err;
+        }
+
+        gt = &key->data.gt;
+        if(!(!sqlite3_bind_text(stmt,     1, gt->ip, -1, SQLITE_STATIC)
+             && !sqlite3_bind_text(stmt,  2, gt->helo, -1, SQLITE_STATIC)
+             && !sqlite3_bind_text(stmt,  3, gt->from, -1, SQLITE_STATIC)
+             && !sqlite3_bind_text(stmt,  4, gt->to, -1, SQLITE_STATIC)))
+        {
+            i_warn("sqlite3_bind_*: %s", sqlite3_errmsg(dbh->db));
+            goto err;
+        }
+        break;
+
+    default:
+        return GREYDB_ERR;
+    }
+
+    ret = sqlite3_step(stmt);
+    switch(ret) {
+    case SQLITE_DONE:
+        /* Not found as there are no rows returned. */
+        res = GREYDB_NOT_FOUND;
+        break;
+
+    case SQLITE_ROW:
+        /* We have a result. */
+        res = GREYDB_FOUND;
+        populate_val(stmt, val, 0);
+        break;
+
+    default:
+        i_warn("unexpected sqlite3_step result: %d", ret);
+        res = GREYDB_ERR;
+        break;
+    }
+
+err:
+    sqlite3_finalize(stmt);
+    return res;
 }
 
 extern int
 Mod_db_del(DB_handle_T handle, struct DB_key *key)
 {
+    struct s3_handle *dbh = handle->dbh;
+    struct Grey_tuple *gt;
+    sqlite3_stmt *stmt;
+    char *sql;
+    int ret;
+
+    switch(key->type) {
+    case DB_KEY_MAIL:
+        sql = "DELETE FROM spamtraps WHERE `address`='?' LIMIT 1";
+        ret = sqlite3_prepare(dbh->db, sql, sizeof(sql), &stmt, NULL);
+        if(ret != SQLITE_OK) {
+            i_warn("sqlite3_prepare: %s", sqlite3_errstr(ret));
+            goto err;
+        }
+
+        ret = sqlite3_bind_text(stmt, 1, key->data.s, -1, SQLITE_STATIC);
+        if(ret != SQLITE_OK) {
+            i_warn("sqlite3_bind_text: %s", sqlite3_errstr(ret));
+            goto err;
+        }
+        break;
+
+    case DB_KEY_IP:
+        sql = "DELETE FROM entries WHERE `ip`='?' "
+            "AND `helo` IS NULL AND `from` IS NULL AND `to` IS NULL "
+            "LIMIT 1";
+        ret = sqlite3_prepare(dbh->db, sql, sizeof(sql), &stmt, NULL);
+        if(ret != SQLITE_OK) {
+            i_warn("sqlite3_prepare: %s", sqlite3_errstr(ret));
+            goto err;
+        }
+
+        ret = sqlite3_bind_text(stmt, 1, key->data.s, -1, SQLITE_STATIC);
+        if(ret != SQLITE_OK) {
+            i_warn("sqlite3_bind_text: %s", sqlite3_errstr(ret));
+            goto err;
+        }
+        break;
+
+    case DB_KEY_TUPLE:
+        sql = "DELETE FROM entries WHERE `ip`='?' "
+            "AND `helo`='?' AND `from`='?' AND `to`='?' "
+            "LIMIT 1";
+        ret = sqlite3_prepare(dbh->db, sql, sizeof(sql), &stmt, NULL);
+        if(ret != SQLITE_OK) {
+            i_warn("sqlite3_prepare: %s", sqlite3_errstr(ret));
+            goto err;
+        }
+
+        gt = &key->data.gt;
+        if(!(!sqlite3_bind_text(stmt,     1, gt->ip, -1, SQLITE_STATIC)
+             && !sqlite3_bind_text(stmt,  2, gt->helo, -1, SQLITE_STATIC)
+             && !sqlite3_bind_text(stmt,  3, gt->from, -1, SQLITE_STATIC)
+             && !sqlite3_bind_text(stmt,  4, gt->to, -1, SQLITE_STATIC)))
+        {
+            i_warn("sqlite3_bind_*: %s", sqlite3_errmsg(dbh->db));
+            goto err;
+        }
+        break;
+
+    default:
+        return GREYDB_ERR;
+    }
+
+    ret = sqlite3_step(stmt);
+    if(ret != SQLITE_DONE) {
+        i_warn("unexpected sqlite3_step result: %d", ret);
+        sqlite3_finalize(stmt);
+        return GREYDB_ERR;
+    }
+    sqlite3_finalize(stmt);
+    return GREYDB_OK;
+
+err:
+    sqlite3_finalize(stmt);
+    DB_rollback_txn(handle);
     return GREYDB_ERR;
 }
 
@@ -338,7 +508,8 @@ Mod_db_get_itr(DB_itr_T itr)
     dbi->stmt = NULL;
     dbi->curr = NULL;
 
-    sql = "SELECT * FROM entries";
+    sql = "SELECT `ip`, `helo`, `from`, `to`, "
+        "`first`, `pass`, `expire`, `bcount`, `pcount` FROM entries";
     ret = sqlite3_prepare(dbh->db, sql, sizeof(sql), &dbi->stmt, NULL);
     if(ret != SQLITE_OK) {
         i_warn("sqlite3_prepare: %s", sqlite3_errstr(ret));
@@ -369,14 +540,116 @@ Mod_db_itr_close(DB_itr_T itr)
 extern int
 Mod_db_itr_next(DB_itr_T itr, struct DB_key *key, struct DB_val *val)
 {
-    /* TODO: call sqlite3_step and populate key/val. */
-    return GREYDB_ERR;
+    struct s3_itr *dbi = itr->dbi;
+    int ret, res;
+
+    ret = sqlite3_step(dbi->stmt);
+    switch(ret) {
+    case SQLITE_DONE:
+        /* We have reached the end as there are no rows. */
+        res = GREYDB_NOT_FOUND;
+        break;
+
+    case SQLITE_ROW:
+        /* We have a result. */
+        res = GREYDB_FOUND;
+        populate_key(dbi->stmt, key, 0);
+        populate_val(dbi->stmt, val, 4);
+        dbi->curr = key;
+        break;
+
+    default:
+        i_warn("unexpected sqlite3_step result: %d", ret);
+        res = GREYDB_ERR;
+        break;
+    }
+
+    return res;
 }
 
 extern int
 Mod_db_itr_replace_curr(DB_itr_T itr, struct DB_val *val)
 {
-    /* TODO: perform UPDATE query. */
+    struct s3_handle *dbh = itr->handle->dbh;
+    struct s3_itr *dbi = itr->dbi;
+    struct DB_key *key = dbi->curr;
+    sqlite3_stmt *stmt;
+    char *sql;
+    struct Grey_tuple *gt;
+    struct Grey_data *gd;
+    int ret;
+
+    switch(key->type) {
+    case DB_KEY_IP:
+        sql = "UPDATE entries SET `first`=?, `pass`=?, `expire`=? "
+            "`bcount`=?, `pcount`=? "
+            "WHERE `ip`='?' AND `helo` IS NULL AND `from` IS NULL "
+            "AND `to` IS NULL "
+            "LIMIT 1";
+        ret = sqlite3_prepare(dbh->db, sql, sizeof(sql), &stmt, NULL);
+        if(ret != SQLITE_OK) {
+            i_warn("sqlite3_prepare: %s", sqlite3_errstr(ret));
+            goto err;
+        }
+
+        gt = &key->data.gt;
+        gd = &val->data.gd;
+        if(!(!sqlite3_bind_int64(stmt,    1, gd->first)
+             && !sqlite3_bind_int64(stmt, 2, gd->pass)
+             && !sqlite3_bind_int64(stmt, 3, gd->expire)
+             && !sqlite3_bind_int(stmt,   4, gd->bcount)
+             && !sqlite3_bind_int(stmt,   5, gd->pcount)
+             && !sqlite3_bind_text(stmt,  6, gt->ip, -1, SQLITE_STATIC)))
+        {
+            i_warn("sqlite3_bind_*: %s", sqlite3_errmsg(dbh->db));
+            goto err;
+        }
+        break;
+
+    case DB_KEY_TUPLE:
+        sql = "UPDATE entries SET `first`=?, `pass`=?, `expire`=? "
+            "`bcount`=?, `pcount`=? "
+            "WHERE `ip`='?' AND `helo`='?' AND `from`='?' AND `to`='?' "
+            "LIMIT 1";
+        ret = sqlite3_prepare(dbh->db, sql, sizeof(sql), &stmt, NULL);
+        if(ret != SQLITE_OK) {
+            i_warn("sqlite3_prepare: %s", sqlite3_errstr(ret));
+            goto err;
+        }
+
+        gt = &key->data.gt;
+        gd = &val->data.gd;
+        if(!(!sqlite3_bind_int64(stmt,    1, gd->first)
+             && !sqlite3_bind_int64(stmt, 2, gd->pass)
+             && !sqlite3_bind_int64(stmt, 3, gd->expire)
+             && !sqlite3_bind_int(stmt,   4, gd->bcount)
+             && !sqlite3_bind_int(stmt,   5, gd->pcount)
+             && !sqlite3_bind_text(stmt,  6, gt->ip, -1, SQLITE_STATIC)
+             && !sqlite3_bind_text(stmt,  7, gt->helo, -1, SQLITE_STATIC)
+             && !sqlite3_bind_text(stmt,  8, gt->from, -1, SQLITE_STATIC)
+             && !sqlite3_bind_text(stmt,  9, gt->to, -1, SQLITE_STATIC)))
+        {
+            i_warn("sqlite3_bind_*: %s", sqlite3_errmsg(dbh->db));
+            goto err;
+        }
+        break;
+
+    default:
+        return GREYDB_ERR;
+    }
+
+    ret = sqlite3_step(stmt);
+    if(ret != SQLITE_DONE) {
+        i_warn("unexpected sqlite3_step result: %d", ret);
+        sqlite3_finalize(stmt);
+        return GREYDB_ERR;
+    }
+    sqlite3_finalize(stmt);
+    return GREYDB_OK;
+
+err:
+    sqlite3_finalize(stmt);
+    DB_rollback_txn(itr->handle);
     return GREYDB_ERR;
 }
 
@@ -389,4 +662,62 @@ Mod_db_itr_del_curr(DB_itr_T itr)
         return DB_del(itr->handle, dbi->curr);
 
     return GREYDB_ERR;
+}
+
+static void
+populate_key(sqlite3_stmt *stmt, struct DB_key *key, int from)
+{
+    struct Grey_tuple *gt;
+    static char buf[(INET6_ADDRSTRLEN + 1) + 4 * (GREY_MAX_MAIL + 1)];
+    char *buf_p = buf;
+
+    memset(key, 0, sizeof(*key));
+    memset(buf, 0, sizeof(buf));
+
+    /* A NULL helo column indicates an IP key type. */
+    key->type = (sqlite3_column_type(stmt, 1) == SQLITE_NULL
+                 ? DB_KEY_IP : DB_KEY_TUPLE);
+    if(key->type == DB_KEY_IP) {
+        key->data.s = buf_p;
+        strncpy(buf_p, sqlite3_column_text(stmt, from + 0),
+                INET6_ADDRSTRLEN + 1);
+    }
+    else {
+        gt = &key->data.gt;
+        gt->ip = buf_p;
+        strncpy(buf_p, sqlite3_column_text(stmt, from + 0),
+                INET6_ADDRSTRLEN + 1);
+        buf_p += INET6_ADDRSTRLEN + 1;
+
+        gt->helo = buf_p;
+        strncpy(buf_p, sqlite3_column_text(stmt, from + 1),
+                GREY_MAX_MAIL + 1);
+        buf_p += GREY_MAX_MAIL + 1;
+
+        gt->from = buf_p;
+        strncpy(buf_p, sqlite3_column_text(stmt, from + 2),
+                GREY_MAX_MAIL + 1);
+        buf_p += GREY_MAX_MAIL + 1;
+
+        gt->to = buf_p;
+        strncpy(buf_p, sqlite3_column_text(stmt, from + 3),
+                GREY_MAX_MAIL + 1);
+        buf_p += GREY_MAX_MAIL + 1;
+
+    }
+}
+
+static void
+populate_val(sqlite3_stmt *stmt, struct DB_val *val, int from)
+{
+    struct Grey_data *gd;
+
+    memset(val, 0, sizeof(*val));
+    val->type = DB_VAL_GREY;
+    gd = &val->data.gd;
+    gd->first = sqlite3_column_int64(stmt,  from + 0);
+    gd->pass = sqlite3_column_int64(stmt,   from + 1);
+    gd->expire = sqlite3_column_int64(stmt, from + 2);
+    gd->bcount = sqlite3_column_int(stmt,   from + 3);
+    gd->pcount = sqlite3_column_int(stmt,   from + 4);
 }
